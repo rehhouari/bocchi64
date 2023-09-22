baeb1590ef7065fe52ca6ba9aa9565fb9f972691
Rafik El Hadi Houari <rehhouari@gmail.com>
2023-09-22 05:13:11 +0100

    update

--- astro.config.ts
+++ astro.config.ts
@@ -24,14 +24,12 @@ export default defineConfig({
 				registerType: "autoUpdate",
 				manifest,
 				workbox: {
-					globDirectory: 'dist',
 					globPatterns: [
 						'**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,html,xml,txt}',
 					],
 					// Don't fallback on document based (e.g. `/some-page`) requests
 					// This removes an errant console.log message from showing up.
 					navigateFallback: null,
-					directoryIndex: "/index.html"
 				},
 			})
 		]
